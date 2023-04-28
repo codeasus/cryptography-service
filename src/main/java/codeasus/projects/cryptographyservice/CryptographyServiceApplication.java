8c527e542d2ae2f609b01ee927318d8fec759b1c
Orhan Rosenfeld <codeasus.orhan@outlook.com>
2023-04-28 23:13:02 +0400

    First commit

---
+++ src/main/java/codeasus/projects/cryptographyservice/CryptographyServiceApplication.java
@@ -0,0 +1,13 @@
+package codeasus.projects.cryptographyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptographyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptographyServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/codeasus/projects/cryptographyservice/CryptographyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package codeasus.projects.cryptographyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptographyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
